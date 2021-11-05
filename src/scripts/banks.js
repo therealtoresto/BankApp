a3391a4946e592ad0b8cb5e7e012d26f4c441b76
Taras Bondaruk <tarasbondaruk@MacBook-Air-Taras.local>
2021-11-05 08:55:45 +0200

    added functional for edit and delete menu

--- src/scripts/banks.js
+++ src/scripts/banks.js
@@ -2,19 +2,53 @@
 
 const table = document.querySelector('.container-table');
 const form = document.querySelector('.container-form');
-const createButton = document.querySelector('.createButton');
+const search = document.querySelector('.container-search');
+const createButton = document.querySelector('.create-button');
+const editButton = document.querySelector('.edit-button');
+const deleteButton = document.querySelector('.delete-button')
 const clearButton = document.querySelector('.button-functional-clear');
 const confirmButton = document.querySelector('.button-functional-confirm');
 const fields = document.querySelectorAll('.field');
 
+search.style.display = 'none';
+form.style.display = 'none';
+
 const createBank = createButton.onclick = () => {
+
     if (table.style.display == '') {
+        search.style.display = 'none';
         table.style.display = 'none';
-        form.style.visibility = 'visible';
-    }
-    else {
+        form.style.display = '';
+    } else {
+        search.style.display = 'none';
         table.style.display = '';
-        form.style.visibility = 'hidden';
+        form.style.display = 'none';
+    }
+}
+
+const editBank = editButton.onclick = () => {
+    if (search.style.display == 'none') {
+        search.style.display = 'flex';
+        table.style.display = '';
+        form.style.display = 'none';
+
+    } else {
+        table.style.display = '';
+        search.style.display = 'none';
+        form.style.display = 'none';
+    }
+}
+
+const deleteBank = deleteButton.onclick = () => {
+    if (search.style.display == 'none') {
+        search.style.display = 'flex';
+        table.style.display = '';
+        form.style.display = 'none';
+
+    } else {
+        table.style.display = '';
+        search.style.display = 'none';
+        form.style.display = 'none';
     }
 }
 
